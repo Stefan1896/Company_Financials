0e636bd40f51c25b0135bbf5850458f21a8f8a67
Stefan Werner <s.werner1896@gmail.com>
2022-03-05 17:43:17 +0100

    Initialize dataset file

---
+++ R/datasets.R
@@ -0,0 +1,5 @@
+#' financials
+#'
+#' Dataset from Kaggle: https://www.kaggle.com/paytonfisher/sp-500-companies-with-financial-information
+#' @format Tibble with 505 features and 14 fields
+"financials"
